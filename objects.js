3c106a50aec8c4e34f9cfc370dc01e1bbe396ed3
ksha1018@naver.com <ksha1018@naver.com>
2018-03-01 12:36:21 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,3 +5,15 @@ function updateObjectWithKeyAndValue(obj, key, value){
 const recipe = {eggs: 3};
 console.log(updateObjectWithKeyAndValue(recipe, 'chocolate', '1 cup'));
 console.log(recipe);
+
+var playlist = {
+  an: 'rain',
+  kwon: 'snow',
+  kim: 'ski'
+}
+
+function updatePlaylist(obj, key, value){
+  return Object.assign({}, obj, {[key]:value});
+}
+
+console.log(updatePlaylist(playlist, 'park', 'glass'));
